8b1aa59c066824ae5432be855ad5dee0a2518ac7
1saulsh <saulsh@yahoo.com>
2018-04-18 14:35:09 -0600

    packages added. trying to remove throw err

--- liri.js
+++ liri.js
@@ -1,69 +1,58 @@
 require("dotenv").config();
 
-console.log("The liri is starting");
+//console.log("The liri is starting");
+//console.log(process.argv);
 
 //Grab data from keys.js using node module imports
 var request = require("request");
 var spotify = require("spotify");
 // var fs reads and writes files
 var fs = require("fs");
-var twitter = require("twitter");
-
-var twitkey = require("./keys.js");
- console.log(keys);
-var client = new Twitter(twitkey.twitterKeys);
-
+var Twitter = require("twitter");
+//load exports from keys.js file which has Twitter auth keys
+var twitKey = require("./keys.js");
+//console.log(twitKey);
+var client = new Twitter(twitKey.twitterKeys);
 
 //Stored argument's array
-
 var inputArgs = process.argv;
 var whatToDo = inputArgs[2];
 var songMovie = inputArgs.slice(3);
 
 directIt(whatToDo);
 
-
-// //movie or song
-// var x = "";
-// //attaches multiple word arguments
-// for (var i=3; i<nodeArgv.length; i++){
-//   if(i>3 && i<nodeArgv.length){
-//     x = x + "+" + nodeArgv[i];
-//   } else{
-//     x = x + nodeArgv[i];
-//   }
-// }
-
+//functions
 function directIt(play) {
 //switch case
 switch(play){
   case "my-tweets":
     showTweets();
-  break;
+    break;
 
   case "spotify-this-song":
     getSong();
-  break;
+    break;
 
   case "movie-this":
     getMovieInfo();
-  break;
+    break;
 
   case "do-what-it-says":
     doThing();
-  break;
+    break;
 
   default:
     console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
     };
 };
 
+//get most recent twitter tweets
 function showTweets(){
-  //Display last 20 Tweets
+  //Twitter API parameters & Display last 20 Tweets
   var params = {screen_name: "heidiBluem", count:20};
-  
+  console.log(client);
   //use Twitter npm to retrieve last 20 tweets
-  client.get("statuses/user_timeline", params, function(error, tweets, response){
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
     if(!error){
         //array to hold tweets
         var twits = []; 
@@ -74,19 +63,20 @@ function showTweets(){
         });
     }
     console.log(twits);
-}else {
+    }else {
     console.log("faiure on twitter" + JSON.stringify(error));
-        };
+    };
   });
 };
 
+//get song from spotify
 function spotifySong() {
-    //console.log("songMovie is " + songMovie);
+    console.log("songMovie is " + songMovie);
     if (songMovie == "") {
-        //console.log("song not entered - using default")
-		songMovie = "The Sign";
+        console.log("song not entered - using default")
+		    songMovie = "The Sign";
     };
-    //console.log("songMovie is " + songMovie);
+    console.log("songMovie is " + songMovie);
     
 	//find the song that was entered on the command line and report song info
     spotify.search({ type: 'track', query: song}, function(error, data){
@@ -96,7 +86,7 @@ function spotifySong() {
     } else {
       //for(var i = 0; i < data.tracks.items.length; i++){
         var songData = data.tracks.items[0];
-        ////console.log("spotify data " + JSON.stringify(songInfo));
+        console.log("spotify data " + JSON.stringify(songInfo));
         //artist
         console.log("Artist: " + songData.artists[0].name);
         //song name
@@ -106,20 +96,11 @@ function spotifySong() {
         //album name
         console.log("Album: " + songData.album.name);
         console.log("-----------------------");
-        
-    //     //adds text to log.txt
-    //     fs.appendFile('log.txt', songData.artists[0].name);
-    //     fs.appendFile('log.txt', songData.name);
-    //     fs.appendFile('log.txt', songData.preview_url);
-    //     fs.appendFile('log.txt', songData.album.name);
-    //     fs.appendFile('log.txt', "-----------------------");
-    //   }
-    // } else{
-    //   console.log('Error occurred.');
     };
   });
 };
 
+//get info about entered movie title from OMDB
 function getMovieInfo() {
     //console.log("Movie entered " + songMovie);
     if (songMovie == "") {
@@ -148,22 +129,12 @@ function getMovieInfo() {
             console.log("Rotten Tomatoes Rating: " + omdbRec.Ratings[i].Value);
         };
     };
-    //   //adds text to log.txt
-    //   fs.appendFile('log.txt', "Title: " + body.Title);
-    //   fs.appendFile('log.txt', "Release Year: " + body.Year);
-    //   fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
-    //   fs.appendFile('log.txt', "Country: " + body.Country);
-    //   fs.appendFile('log.txt', "Language: " + body.Language);
-    //   fs.appendFile('log.txt', "Plot: " + body.Plot);
-    //   fs.appendFile('log.txt', "Actors: " + body.Actors);
-    //   fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
-    //   fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
-
-    console.log("Rotten Tomatoes URL: " + omdbRec.tomatoURL);
-} else {
-    console.log("Error in OMDB call");
-};
-});
+    
+      console.log("Rotten Tomatoes URL: " + omdbRec.tomatoURL);
+    }else {
+      console.log("Error in OMDB call");
+        };
+    });
 };
     
     if(movie === "Mr. Nobody"){
@@ -179,6 +150,8 @@ function getMovieInfo() {
 
 function doThing(){
   fs.readFile('random.txt', "utf8", function(error, data){
+    //split the data by the comma so you can access the first part which is which type of search we are doing and the second part which is the userSelection of what we are looking up
+	var things = data.split(',');
     var splitData= data.split(",");
     whatToDo = splitData[0];
 	songMovie = splitData[1];
